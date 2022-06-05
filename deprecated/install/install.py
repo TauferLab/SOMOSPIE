6468b8ddfe07b398d72731ce9b8fb64c6952a308
Paula Olaya <polaya07@gmail.com>
2022-06-05 14:11:06 -0400

    Remove deprecated directory

--- deprecated/install/install.py
+++
@@ -1,9 +0,0 @@
-def buildFontCache():
-    import matplotlib
-    matplotlib
-    matplotlib.use('AGG')
-    from matplotlib import pyplot as plt
-    plt.plot([0],[0])
-    plt.show()
-    plt.clf()
-

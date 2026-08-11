b2d3c767b2229f94f960a96d547ef8377e3ceadd
Chandler <jweeks12@vols.utk.edu>
2026-08-11 19:44:28 +0000

    Initialize Structure

---
+++ SOMOSPIE_GFM/src/somospie_gfm/acquisition/retrieve_sm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+year=$1
+version=9.2
+sm_dir="/work/hdd/bebq/jweeks2/prithvi_workflow/ESA_CCI"
+
+if [ ! -d ${sm_dir}/${year} ]; then
+    mkdir -p ${sm_dir}/${year}
+
+    for month in $(seq -f "%02g" 1 12); do
+        for day in $(seq -f "%02g" 1 31); do
+            wget ftp://anon-ftp.ceda.ac.uk/neodc/esacci/soil_moisture/data/daily_files/COMBINED/v0${version}/${year}/ESACCI-SOILMOISTURE-L3S-SSMV-COMBINED-${year}${month}${day}000000-fv0${version}.nc -nc -t 1 -P ${sm_dir}/${year}/
+        done
+    done
+fi
